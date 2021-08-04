4f5a69fa69467ef2655aa92857c9054574379c94
Catherine Radin <73500682+CatRadin@users.noreply.github.com>
2021-08-04 18:40:10 -0500

    Added state

--- zesty/src/App.js
+++ zesty/src/App.js
@@ -1,18 +1,42 @@
 
 import './App.css';
+import React, {useState} from "react"
 import cabbage from './assects/image1.jpeg'
+import mango from './assects/image2.jpeg'
+import fig from './assects/image3.jpeg'
+import gaze from './assects/image4.jpeg'
+import peach from './assects/image5.jpeg'
+import avocado from './assects/image1.jpeg'
+
+
+const images = [cabbage, mango, fig, gaze, peach, avocado]
+
 
 function App() {
+  
+  const [currentImage, setCurrentImage] = useState(0);
+  const length = images.length -1;
 
-  const images = []
+  const handleClick = () => {
+    setCurrentImage((currentImage) => {
+        if(currentImage < length){
+            return currentImage + 1
+        } else {
+            return 0;
+        }
+    })
+  }
 
   return (
     <div className="App">
       <div className='Title'>
       <h1>Zesty</h1>
       <h2>A photography project <br /> by Ella Fieldling</h2>
+      
       </div>
-      <div className='image-container'><img alt="" src =""/></div>
+      <div className='image-container'>
+        <h3>{currentImage + 1} / {images.length}</h3>
+        <img alt="" src ={images[currentImage]} onClick={handleClick}/></div>
     </div>
   );
 }
